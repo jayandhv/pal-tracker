4e8b572fd0646419af5b5ce25d6b3ff95fe5155b
Ubuntu <ubuntu@ip-172-31-5-147.ap-southeast-1.compute.internal>
2021-07-01 04:41:15 +0000

    Spring JDBC implementation

--- src/main/java/io/pivotal/pal/tracker/BeanConfiguration.java
+++ src/main/java/io/pivotal/pal/tracker/BeanConfiguration.java
@@ -1,6 +1,9 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
 
 @org.springframework.context.annotation.Configuration
 public class BeanConfiguration {
@@ -9,5 +12,8 @@ public class BeanConfiguration {
     public TimeEntryRepository timeEntryRepository() {
         return new InMemoryTimeEntryRepository();
     }
+
+    @Bean
+    public DataSource dataSource() { return DataSourceBuilder.create().build(); }
 
 }
